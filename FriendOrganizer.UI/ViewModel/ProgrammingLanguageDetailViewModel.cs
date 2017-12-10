7c1da58aca34c26aa2a91a69780c423ae8ca2177
Sergej Schmal <serg.schmal@gmx.de>
2017-12-10 11:21:36 +0100

    Test

---
+++ FriendOrganizer.UI/ViewModel/ProgrammingLanguageDetailViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using FriendOrganizer.UI.Data.Repositories;
+using FriendOrganizer.UI.View.Services;
+using FriendOrganizer.UI.Wrapper;
+using Prism.Commands;
+using Prism.Events;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class ProgrammingLanguageDetailViewModel : DetailViewModelBase
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageDetailViewModel(IEventAggregator eventAggregator, IMessageDialogService messageDialogService,
+            IProgrammingLanguageRepository programmingLanguageRepository) : base(eventAggregator, messageDialogService)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+            Title = "Programming Languages";
+            Title = "Programming Languages";
+            ProgrammingLanguages = new ObservableCollection<ProgrammingLanguageWrapper>();
+        }
+
+        public ObservableCollection<ProgrammingLanguageWrapper> ProgrammingLanguages { get; }
+
+        public override async Task LoadAsync(int id)
+        {
+            Id = id;
+
+            foreach (var wrapper in ProgrammingLanguages)
+            {
+                wrapper.PropertyChanged -= Wrapper_PropertyChanged;
+            }
+
+            ProgrammingLanguages.Clear();
+
+            var languages = await _programmingLanguageRepository.GetAllAsync();
+
+            foreach (var model in languages)
+            {
+                var wrapper = new ProgrammingLanguageWrapper(model);
+                wrapper.PropertyChanged += Wrapper_PropertyChanged;
+                ProgrammingLanguages.Add(wrapper);
+            }
+        }
+
+        private void Wrapper_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (!HasChanges)
+            {
+                HasChanges = _programmingLanguageRepository.HasChanges();
+            }
+            if (e.PropertyName == nameof(ProgrammingLanguageWrapper.HasErrors))
+            {
+                ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+        protected override bool OnSaveCanExecute()
+        {
+            return HasChanges && ProgrammingLanguages.All(p => !p.HasErrors);
+        }
+
+        protected override async void OnSaveExecute()
+        {
+            await _programmingLanguageRepository.SaveAsync();
+            HasChanges = _programmingLanguageRepository.HasChanges();
+        }
+
+        protected override void OnDeleteExecute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
